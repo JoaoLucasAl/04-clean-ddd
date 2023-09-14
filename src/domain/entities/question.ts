e4fd5013a0010856578974c5ac6c9eb7f6d66967
João Lucas <joaolucas.power@gmail.com>
2023-09-14 15:18:41 -0300

    :tada: First commit

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,26 @@
+// @ts-ignore
+import { randomUUID } from "bun:crypto";
+
+interface QuestionProps {
+    title: string, 
+    content: string,
+    slug: string, 
+    authorId: string,
+}
+
+export class Question {
+    public id: string;
+    public title: string;
+    public slug: string;
+    public content: string;
+    public authorId: string;
+
+
+    constructor(props: QuestionProps, id?: string) {
+        this.title = props.title;
+        this.content = props.content;
+        this.slug = props.slug;
+        this.authorId = props.authorId;
+        this.id = id ?? randomUUID();
+    }
+}
---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,25 @@
+// @ts-ignore
+import { expect, test } from 'bun:test'
+
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answers-repository';
+import { Answer } from '../entities/answer';
+
+const fakeAnswerRepository: AnswerRepository = {
+    create: async (answer: Answer) => {
+        return
+    }
+}
+
+test('create an answer', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+    const answer = await answerQuestion.execute({
+        content: 'any_content', 
+        instructorId: 'any_instructor_id', 
+        questionId: 'any_question_id'
+    });
+    
+
+    expect(answer.content).toEqual('any_content');
+})
