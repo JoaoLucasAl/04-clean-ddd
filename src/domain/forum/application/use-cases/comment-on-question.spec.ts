a739a351eca648bd2c0e7092cb013ae3891adf64
João Lucas <joaolucas.power@gmail.com>
2023-12-13 12:44:01 +0000

    :sparkles: feat: Comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { CommentOnQuestionUseCase } from './commnet-on-question'
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Commnet on Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on question', async () => {
+    const question = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'Comment content',
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items[0].questionId).toEqual({
+      value: question.id.toString(),
+    })
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Comment content',
+    )
+  })
+})
